950337056b02d0effa8a764cd2c358a363f8ef71
Andrew Rivett <veggiefrog@gmail.com>
2017-04-03 16:36:10 -0400

    Fixed bug with postal

--- public/registrationGet.php
+++ public/registrationGet.php
@@ -180,7 +180,7 @@ function ciniki_musicfestivals_registrationGet($ciniki) {
                 if( $competitor['address'] != '' ) { $address .= $competitor['address']; }
                 $city = $competitor['city'];
                 if( $competitor['province'] != '' ) { $city .= ($city != '' ? ", " : '') . $competitor['province']; }
-                if( $competitor['postal'] != '' ) { $postal .= ($postal != '' ? "  " : '') . $competitor['postal']; }
+                if( $competitor['postal'] != '' ) { $city .= ($city != '' ? "  " : '') . $competitor['postal']; }
                 if( $city != '' ) { $address .= ($address != '' ? "\n" : '' ) . $city; }
                 if( $address != '' ) {
                     $details[] = array('label'=>'Address', 'value'=>$address);
