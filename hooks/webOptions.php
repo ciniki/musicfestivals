fb4f6005b8ad109c0186c0d3d8f29f2b3370e4bf
Andrew Rivett <veggiefrog@gmail.com>
2020-03-02 11:59:47 -0500

    Added webOptions hooks so this can be a module available to custom pages

---
+++ hooks/webOptions.php
@@ -0,0 +1,48 @@
+<?php
+//
+// Description
+// -----------
+// This function will return the list of options for the module that can be set for the website.
+//
+// Arguments
+// ---------
+// ciniki:
+// settings:        The web settings structure.
+// tnid:     The ID of the tenant to get musicfestivals for.
+//
+// args:            The possible arguments for posts
+//
+//
+// Returns
+// -------
+//
+function ciniki_musicfestivals_hooks_webOptions(&$ciniki, $tnid, $args) {
+
+    //
+    // Check to make sure the module is enabled
+    //
+    if( !isset($ciniki['tenant']['modules']['ciniki.musicfestivals']) ) {
+        return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.musicfestivals.2', 'msg'=>"I'm sorry, the page you requested does not exist."));
+    }
+
+    //
+    // Get the settings from the database
+    //
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbDetailsQueryDash');
+    $rc = ciniki_core_dbDetailsQueryDash($ciniki, 'ciniki_web_settings', 'tnid', $tnid, 'ciniki.web', 'settings', 'page-musicfestivals');
+    if( $rc['stat'] != 'ok' ) {
+        return $rc;
+    }
+    if( !isset($rc['settings']) ) {
+        $settings = array();
+    } else {
+        $settings = $rc['settings'];
+    }
+
+
+    $options = array();
+    $pages['ciniki.musicfestivals.current'] = array('name'=>'Music Festivals', 'options'=>$options);
+
+    return array('stat'=>'ok', 'pages'=>$pages);
+}
+?>
