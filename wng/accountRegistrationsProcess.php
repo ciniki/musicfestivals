88f2bd9a566747e50dde05688c5d3869a9c13d27
Andrew Rivett <veggiefrog@gmail.com>
2023-03-24 10:42:39 -0400

    Fixed bug with past reg showing in account

--- wng/accountRegistrationsProcess.php
+++ wng/accountRegistrationsProcess.php
@@ -1167,6 +1167,7 @@ function ciniki_musicfestivals_wng_accountRegistrationsProcess(&$ciniki, $tnid, 
                 . ") "
             . "WHERE registrations.billing_customer_id = '" . ciniki_core_dbQuote($ciniki, $request['session']['customer']['id']) . "' "
             . "AND registrations.tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
+            . "AND registrations.festival_id = '" . ciniki_core_dbQuote($ciniki, $festival['id']) . "' "
             . "";
         ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryArrayTree');
         $rc = ciniki_core_dbHashQueryArrayTree($ciniki, $strsql, 'ciniki.musicfestivals', array(
