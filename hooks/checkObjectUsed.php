7aa7cb914fa2fc42bde3d68ca074511dd4d76733
Andrew Rivett <veggiefrog@gmail.com>
2024-02-06 18:44:52 -0500

    Added check for customer when tied to teacher, billing or accompanist in registration

--- hooks/checkObjectUsed.php
+++ hooks/checkObjectUsed.php
@@ -36,6 +36,27 @@ function ciniki_musicfestivals_hooks_checkObjectUsed($ciniki, $tnid, $args) {
             $count = $rc['num']['items'];
             $msg .= ($msg!=''?' ':'') . "There " . ($count==1?'is':'are') . " $count music festival adjudicator" . ($count==1?'':'s') . " for this customer.";
         }
+        //
+        // Check the invoice customers
+        //
+        $strsql = "SELECT 'items', COUNT(*) "
+            . "FROM ciniki_musicfestival_registrations "
+            . "WHERE ("
+                . "billing_customer_id = '" . ciniki_core_dbQuote($ciniki, $args['object_id']) . "' "
+                . "OR teacher_customer_id = '" . ciniki_core_dbQuote($ciniki, $args['object_id']) . "' "
+                . "OR accompanist_customer_id = '" . ciniki_core_dbQuote($ciniki, $args['object_id']) . "' "
+                . ") "
+            . "AND tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
+            . "";
+        $rc = ciniki_core_dbCount($ciniki, $strsql, 'ciniki.musicfestivals', 'num');
+        if( $rc['stat'] != 'ok' ) {
+            return $rc;
+        }
+        if( isset($rc['num']['items']) && $rc['num']['items'] > 0 ) {
+            $used = 'yes';
+            $count = $rc['num']['items'];
+            $msg .= ($msg!=''?' ':'') . "There " . ($count==1?'is':'are') . " $count music festival registration" . ($count==1?'':'s') . " for this customer.";
+        }
     }
 
     return array('stat'=>'ok', 'used'=>$used, 'count'=>$count, 'msg'=>$msg);
