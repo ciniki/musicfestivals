3eb29d611796d28200dca7619dfa50ac734cd536
Andrew Rivett <veggiefrog@gmail.com>
2024-03-05 06:38:23 -0500

    Fixed bug with registration search not showing titles

--- public/registrationSearch.php
+++ public/registrationSearch.php
@@ -68,21 +68,49 @@ function ciniki_musicfestivals_registrationSearch($ciniki) {
         . "registrations.class_id, "
         . "classes.code AS class_code, "
         . "classes.name AS class_name, "
-        . "registrations.title1, "
-        . "registrations.perf_time1, "
-        . "registrations.title2, "
-        . "registrations.perf_time2, "
-        . "registrations.title3, "
-        . "registrations.perf_time3, "
         . "FORMAT(registrations.fee, 2) AS fee, "
         . "registrations.payment_type, "
         . "registrations.participation, "
+        . "registrations.title1, "
+        . "registrations.title2, "
+        . "registrations.title3, "
+        . "registrations.title4, "
+        . "registrations.title5, "
+        . "registrations.title6, "
+        . "registrations.title7, "
+        . "registrations.title8, "
+        . "registrations.composer1, "
+        . "registrations.composer2, "
+        . "registrations.composer3, "
+        . "registrations.composer4, "
+        . "registrations.composer5, "
+        . "registrations.composer6, "
+        . "registrations.composer7, "
+        . "registrations.composer8, "
+        . "registrations.movements1, "
+        . "registrations.movements2, "
+        . "registrations.movements3, "
+        . "registrations.movements4, "
+        . "registrations.movements5, "
+        . "registrations.movements6, "
+        . "registrations.movements7, "
+        . "registrations.movements8, "
         . "registrations.video_url1, "
         . "registrations.video_url2, "
         . "registrations.video_url3, "
+        . "registrations.video_url4, "
+        . "registrations.video_url5, "
+        . "registrations.video_url6, "
+        . "registrations.video_url7, "
+        . "registrations.video_url8, "
         . "registrations.music_orgfilename1, "
         . "registrations.music_orgfilename2, "
-        . "registrations.music_orgfilename3 "
+        . "registrations.music_orgfilename3, "
+        . "registrations.music_orgfilename4, "
+        . "registrations.music_orgfilename5, "
+        . "registrations.music_orgfilename6, "
+        . "registrations.music_orgfilename7, "
+        . "registrations.music_orgfilename8 "
         . "FROM ciniki_musicfestival_competitors AS competitors "
         . "LEFT JOIN ciniki_musicfestival_registrations AS registrations ON ("
             . "(competitors.id = registrations.competitor1_id "
@@ -132,9 +160,15 @@ function ciniki_musicfestivals_registrationSearch($ciniki) {
             'fields'=>array('id', 'festival_id', 'teacher_customer_id', 'teacher_name', 'billing_customer_id', 'rtype', 'rtype_text', 
                 'status', 'status_text', 'invoice_id', 'display_name', 
                 'class_id', 'class_code', 'class_name', 
-                'title1', 'perf_time1', 'title2', 'perf_time2', 'title3', 'perf_time3', 'fee', 'payment_type', 
-                'participation', 'video_url1', 'video_url2', 'video_url3', 
-                'music_orgfilename1', 'music_orgfilename2', 'music_orgfilename3',
+                'fee', 'payment_type', 'participation', 
+                'title1', 'composer1', 'movements1', 'video_url1', 'music_orgfilename1',
+                'title2', 'composer2', 'movements2', 'video_url2', 'music_orgfilename2',
+                'title3', 'composer3', 'movements3', 'video_url3', 'music_orgfilename3',
+                'title4', 'composer4', 'movements4', 'video_url4', 'music_orgfilename4',
+                'title5', 'composer5', 'movements5', 'video_url5', 'music_orgfilename5',
+                'title6', 'composer6', 'movements6', 'video_url6', 'music_orgfilename6',
+                'title7', 'composer7', 'movements7', 'video_url7', 'music_orgfilename7',
+                'title8', 'composer8', 'movements8', 'video_url8', 'music_orgfilename8',
                 ),
             'maps'=>array(
                 'rtype_text'=>$maps['registration']['rtype'],
@@ -150,8 +184,20 @@ function ciniki_musicfestivals_registrationSearch($ciniki) {
     if( isset($rc['registrations']) ) {
         $registrations = $rc['registrations'];
         $registration_ids = array();
-        foreach($registrations as $iid => $registration) {
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'musicfestivals', 'private', 'titleMerge');
+        foreach($registrations as $rid => $registration) {
             $registration_ids[] = $registration['id'];
+            $registrations[$rid]['titles'] = '';
+            for($i = 1; $i <= 8; $i++) {
+                if( $registration["title{$i}"] != '' ) {
+                    $rc = ciniki_musicfestivals_titleMerge($ciniki, $args['tnid'], $registration, $i);
+                    if( $rc['stat'] == 'ok' ) {
+                        $registrations[$rid]["title{$i}"] = $rc['title'];
+                        $registrations[$rid]['titles'] .= ($registrations[$rid]['titles'] != '' ? '<br/>' : '') . $rc['title'];
+                    }
+                }
+            }
+
         }
     } else {
         $registrations = array();
