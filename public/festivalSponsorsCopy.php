6084574583aced01c07894d13f3f36d98668b5c2
Andrew Rivett <veggiefrog@gmail.com>
2024-09-17 22:43:09 -0400

    Fixed sponsors copy to also copy tags

--- public/festivalSponsorsCopy.php
+++ public/festivalSponsorsCopy.php
@@ -76,29 +76,57 @@ function ciniki_musicfestivals_festivalSponsorsCopy($ciniki) {
         . "sponsors.url, "
         . "sponsors.sequence, "
         . "sponsors.flags, "
-        . "sponsors.image_id "
+        . "sponsors.image_id, "
+        . "tags.id AS tag_id, "
+        . "tags.tag_type, "
+        . "tags.tag_name, "
+        . "tags.permalink "
         . "FROM ciniki_musicfestival_sponsors AS sponsors "
+        . "LEFT JOIN ciniki_musicfestival_sponsor_tags AS tags ON ("
+            . "sponsors.id = tags.sponsor_id "
+            . "AND tags.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
+            . ") "
         . "WHERE sponsors.festival_id = '" . ciniki_core_dbQuote($ciniki, $args['old_festival_id']) . "' "
         . "AND sponsors.tnid = '" . ciniki_core_dbQuote($ciniki, $args['tnid']) . "' "
         . "";
     ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryArrayTree');
     $rc = ciniki_core_dbHashQueryArrayTree($ciniki, $strsql, 'ciniki.musicfestivals', array(
-        array('container'=>'sponsors', 'fname'=>'id', 'fields'=>array('id', 'name', 'url', 'sequence', 'flags', 'image_id')),
+        array('container'=>'sponsors', 'fname'=>'id', 
+            'fields'=>array('name', 'url', 'sequence', 'flags', 'image_id'),
+            ),
+        array('container'=>'tags', 'fname'=>'tag_id', 
+            'fields'=>array('tag_type', 'tag_name', 'permalink'),
+            ),
         ));
     if( $rc['stat'] != 'ok' ) {
         return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.musicfestivals.839', 'msg'=>'Unable to load sponsors', 'err'=>$rc['err']));
     }
     $sponsors = isset($rc['sponsors']) ? $rc['sponsors'] : array();
 
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'objectAdd');
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'tagsUpdate');
+
     //
     // Add sponsors to new festival
     //
     foreach($sponsors AS $sponsor) {
         $sponsor['festival_id'] = $args['festival_id'];
-        ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'objectAdd');
         $rc = ciniki_core_objectAdd($ciniki, $args['tnid'], 'ciniki.musicfestivals.sponsor', $sponsor, 0x04);
         if( $rc['stat'] != 'ok' ) {
             return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.musicfestivals.841', 'msg'=>'Unable to add the sponsor', 'err'=>$rc['err']));
+        }
+        $sponsor_id = $rc['id'];
+        if( isset($sponsor['tags']) ) {
+            $tags = array();
+            foreach($sponsor['tags'] as $tag) {
+                $tags[] = $tag['tag_name'];
+            }
+            $rc = ciniki_core_tagsUpdate($ciniki, 'ciniki.musicfestivals', 'tag', $args['tnid'],
+                'ciniki_musicfestival_sponsor_tags', 'ciniki_musicfestivals_history',
+                'sponsor_id', $sponsor_id, 10, $tags);
+            if( $rc['stat'] != 'ok' ) {
+                return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.musicfestivals.841', 'msg'=>'Unable to add the sponsor tag', 'err'=>$rc['err']));
+            }
         }
     }
 
