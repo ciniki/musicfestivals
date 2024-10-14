fabefb736f715689bbc7bf8bbb8904ef8f088a2f
Andrew Rivett <veggiefrog@gmail.com>
2024-10-13 22:52:02 -0400

    Removed debug code

--- sapos/itemUpdate.php
+++ sapos/itemUpdate.php
@@ -12,14 +12,12 @@
 //
 function ciniki_musicfestivals_sapos_itemUpdate($ciniki, $tnid, $invoice_id, $item) {
 
-    error_log('iteUpdate');
     if( isset($item['object']) && $item['object'] == 'ciniki.musicfestivals.registration' 
         && isset($item['object_id']) && $item['object_id'] > 0 
         ) {
         //
         // Check if extra fee needs to be added
         //
-        error_log('check fees');
         ciniki_core_loadMethod($ciniki, 'ciniki', 'musicfestivals', 'sapos', 'registrationExtraFeesCheck');
         $rc = ciniki_musicfestivals_sapos_registrationExtraFeesCheck($ciniki, $tnid, [
             'registration_id' => $item['object_id'],
