1b7460bdb341b616b79a4d1a0af9ff3665fcd7f8
Andrew Rivett <veggiefrog@gmail.com>
2024-12-06 12:01:58 -0500

    Fixing bug with checkout and members

--- sapos/cartItemCheck.php
+++ sapos/cartItemCheck.php
@@ -54,6 +54,8 @@ function ciniki_musicfestivals_sapos_cartItemCheck($ciniki, $tnid, $customer, $a
                 . ") "
             . "WHERE registrations.id = '" . ciniki_core_dbQuote($ciniki, $args['object_id']) . "' "
             . "AND registrations.tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
+            . "ORDER BY members.id DESC "
+            . "LIMIT 1 "
             . "";
         $rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.musicfestivals', 'registration');
         if( $rc['stat'] != 'ok' ) {
