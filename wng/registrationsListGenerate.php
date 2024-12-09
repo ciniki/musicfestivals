a2532575486db61e7b113d4cc257985548b68774
Andrew Rivett <veggiefrog@gmail.com>
2024-12-09 13:10:14 -0500

    Added sorting to regsitrationListsGenerate. Changed section description to text from varchar.

--- wng/registrationsListGenerate.php
+++ wng/registrationsListGenerate.php
@@ -213,7 +213,7 @@ function ciniki_musicfestivals_wng_registrationsListGenerate(&$ciniki, $tnid, &$
     }
     $strsql .= "AND registrations.tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
         . "AND registrations.festival_id = '" . ciniki_core_dbQuote($ciniki, $festival['id']) . "' "
-        . "ORDER BY registrations.display_name "
+        . "ORDER BY registrations.display_name, registrations.date_added "
         . "";
     ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryArrayTree');
     $rc = ciniki_core_dbHashQueryArrayTree($ciniki, $strsql, 'ciniki.musicfestivals', array(
