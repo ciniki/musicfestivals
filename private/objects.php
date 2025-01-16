185f091e275a64700b3c9bbbda3b5d6decfacab4
Andrew Rivett <veggiefrog@gmail.com>
2025-01-16 15:28:51 -0500

    Fixed bug adding timeslot

--- private/objects.php
+++ private/objects.php
@@ -431,7 +431,7 @@ function ciniki_musicfestivals_objects($ciniki) {
         'table'=>'ciniki_musicfestival_schedule_timeslots',
         'fields'=>array(
             'festival_id'=>array('name'=>'Festival', 'ref'=>'ciniki.musicfestivals.festival'),
-            'ssection_id'=>array('name'=>'Section', 'ref'=>'ciniki.musicfestivals.schedulesection'),
+            'ssection_id'=>array('name'=>'Section', 'ref'=>'ciniki.musicfestivals.schedulesection', 'default'=>'0'),
             'sdivision_id'=>array('name'=>'Division', 'ref'=>'ciniki.musicfestivals.scheduledivision'),
             'slot_time'=>array('name'=>'Time'),
 //            'class1_id'=>array('name'=>'Class 1', 'ref'=>'ciniki.musicfestivals.class', 'default'=>'0'),
