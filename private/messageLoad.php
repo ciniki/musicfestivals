06f263444c698bf038d969c08a68524ca7e5911d
Andrew Rivett <veggiefrog@gmail.com>
2025-02-16 17:18:43 -0500

    Fixed slow load in messageLoad

--- private/messageLoad.php
+++ private/messageLoad.php
@@ -690,7 +690,7 @@ function ciniki_musicfestivals_messageLoad(&$ciniki, $tnid, $args) {
             . "INNER JOIN ciniki_customers AS customers ON ("
                 . "("
                     . "registrations.teacher_customer_id = customers.id "
-                    . "OR registrations.teacher2_customer_id = customers.id "
+//                    . "OR registrations.teacher2_customer_id = customers.id "
                     . ") "
                 . "AND customers.tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
                 . ") "
